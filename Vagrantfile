e4eebaf717c035f8e45baea5a230ae387c8aed20
Adrian Kappel <akappel@ezesoft.com>
2017-05-16 16:32:39 -0400

    Init commit

---
+++ Vagrantfile
@@ -0,0 +1,17 @@
+$private_ip_prefix = '192.168.50'
+
+Vagrant.configure("2") do |config|
+  config.vm.box = "puppetlabs/ubuntu-14.04-64-nocm"
+
+  config.vm.define "jenkins_box" do |j|
+    j.vm.network "private_network", ip: "#{$private_ip_prefix}.6"
+    # j.vm.network "forwarded_port", guest: 8080, host: 8080, host_ip: "127.0.0.1"
+
+    j.vm.provider "vmware_workstation" do |v|
+      v.vmx["memsize"] = "2048"
+      v.vmx["numvcpus"] = "2"
+    end
+
+    j.vm.provision "shell", path: "docker-provision-jenkins.sh"
+  end
+end
---
+++ docker-provision-jenkins.sh
@@ -0,0 +1,20 @@
+#!/usr/bin/env bash
+
+apt-get update
+apt-get install -y curl
+
+# install docker
+curl -sSL https://get.docker.com | sh
+
+# setup users
+useradd jenkins
+usermod -aG docker jenkins
+usermod -aG docker vagrant
+
+docker build -t jenkinsdocker ./jenkinsdocker
+
+# start jenkins with proper command line args
+docker run -itd --name jenkins -p 8080:8080 -p 50000:50000 \
+                      -v /vagrant/jenkins_home:/var/jenkins_home \
+                      -v /var/run/docker.sock:/var/run/docker.sock
+                      jenkinsdocker
